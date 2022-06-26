5c7b75ebaabb8507b32405c1d2b53971153b9546
Carlos Carrasquillo <96191578+CarrasquilloCarlos@users.noreply.github.com>
2022-06-26 10:30:24 -0400

    Update script1.js

--- JS/script1.js
+++ JS/script1.js
@@ -1,1 +1,76 @@
+var nameError=document.getElementById('name-error');
+var phoneError=document.getElementById('phone-error');
+var emailError=document.getElementById('email-error');
+var messageError=document.getElementById('message-error');
 
+function validateName(){
+       var name = document.getElementById('name').value;
+
+       if(name.length == 0){
+        nameError.innerHTML = 'Se requiere el nombre completo';
+        return false;
+       }
+       if(!name.match(/\D[A-Za-zÁÉÍÓÚáéíóú]{3}/)){
+        nameError.innerHTML = 'Escribe nombre';
+        return false;
+       }
+       nameError.innerHTML = 'valido';
+       return true;
+
+}
+function validatePhone(){
+    var phone = document.getElementById('phone').value;
+
+    if(phone.length == 0){
+        phoneError.innerHTML = "Se requiere numero de telefono";
+        return false;
+    }
+    if (phone.length !== 10){
+        phoneError.innerHTML = "Numero de telefono de 10 digitos";
+        return false;
+    }
+    if (!phone.match(/^[0-9]{10}$/)){
+        phoneError.innerHTML= "Se requiere numero de telefono";
+        return false;
+    }
+    phoneError.innerHTML = 'Valido'
+        return true;
+
+}
+function validateEmail(){
+    var email = document.getElementById('email').value;
+
+    if(email.length == 0){
+        emailError.innerHTML = 'Se requiere Email'
+        return false;
+    }
+    if(!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)){
+        emailError.innerHTML = "Email invalido"
+        return false;
+    }
+    emailError.innerHTML = "Valido";
+    return true;
+
+}
+function validateMessage(){
+    var message = document.getElementById('message').value;
+    var required = 30;
+    var left = required - message.length;
+
+    if(left > 0){
+        messageError.innerHTML = left + "Se requiren más caracteres";
+        return false;
+    }
+
+    messageError.innerHTML = "Valido";
+    return true;
+
+}
+function validateForm(){
+    if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage()){
+        submitError.style.display = "block";
+        submitError.innerHTML = "Arreglar error antes de pulsar Submit";
+        setTimeout(function(){submitError.style.display = "none';"}, 3000);
+        return false;
+    }
+}
